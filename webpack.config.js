4c02cb8a15c10b7ebac4534580dfaf730714b982
IFA-01 <bogatyr.oleg@yahoo.com>
2025-11-28 16:13:27 +0300

    trying to configure gh pages correctly

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/HomeWork06/',
+    publicPath: '/HomeWork-6-deploy/',
   },
 
   plugins: [
